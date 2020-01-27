d97804939c3aa82bfdb86bd31d8216e020c5aa21
Pietrzi <pietrzi@op.pl>
2020-01-27 15:36:22 +0100

    changed car to class

--- racing-game/js/Main.js
+++ racing-game/js/Main.js
@@ -1,5 +1,7 @@
 var canvas, canvasContext;
 
+var blueCar = new carClass();
+
 window.onload = function() {
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
@@ -15,7 +17,7 @@ function imageLoadingDoneSoStartGame() {
 	setInterval(updateAll, 1000/framesPerSecond);
 
 	setupInput();
-	carReset();
+	blueCar.reset();
 }
 
 function updateAll() {
@@ -24,11 +26,11 @@ function updateAll() {
 }
 
 function moveAll() {
-	carMove();
-	carTrackHandling();
+	blueCar.move();
+	carTrackHandling(blueCar);
 }
 
 function drawAll() {
 	drawTracks();
-	carDraw();
-} 
+	blueCar.draw();
+}
