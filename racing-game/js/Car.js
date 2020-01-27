6257354bafc780986219e5d4565bfa22eeea500a
Pietrzi <pietrzi@op.pl>
2020-01-27 21:52:17 +0100

    finished

--- racing-game/js/Car.js
+++ racing-game/js/Car.js
@@ -32,6 +32,7 @@ function carClass() {
 	this.reset = function(whichImage, carName) {
 		this.name = carName;
 		this.myCarPic = whichImage;
+		this.speed = 0;
 
 		for(var eachRow=0;eachRow<TRACK_ROWS;eachRow++) {
 			for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
@@ -45,6 +46,7 @@ function carClass() {
 				} // end of player start if
 			} // end of col for
 		} // end of row for
+		console.log("NO PLAYER START FOUND!");
 	} // end of carReset func
 
 	this.move = function() {
