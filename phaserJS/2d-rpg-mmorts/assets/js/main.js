e22860fd9467d79f66f41aca7426a347beb20e98
Pietrzi <pietrzi@op.pl>
2020-06-04 18:07:21 +0200

    proper structure after very basics

--- phaserJS/2d-rpg-mmorts/assets/js/main.js
+++ phaserJS/2d-rpg-mmorts/assets/js/main.js
@@ -1,59 +1,24 @@
 const config = {
-type: Phaser.AUTO,
-width: 800,
-height: 600,
-scene: {
-    preload: preload,
-    create: create,
-    update: update
-},
-physics: {
-    default: 'arcade',
-    arcade: {
-        debug: true,
-        gravity: {
-            y: 0,
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: [
+        BootScene,
+        TitleScene,
+        GameScene,
+        UiScene
+    ],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                y: 0,
+            }
         }
     }
-}
-};
+    };
+    
+    const game = new Phaser.Game(config);
 
-const game = new Phaser.Game(config);
-
-function preload() {
-    this.load.image('button1', 'assets/images/ui/blue_button01.png');
-    this.load.spritesheet('items', 'assets/images/items.png', { frameWidth: 32, frameHight: 32 });
-    this.load.spritesheet('characters', 'assets/images/characters.png', { frameWidth: 32, frameHight: 32 });
-}
-
-function create() {
-    const button = this.add.image(100, 100, 'button1');
-    button.setOrigin(0.5, 0.5);
-
-    this.add.sprite(300, 100, 'button1');
-
-    this.add.image(300, 300, 'items', 2);
-
-    this.physics.add.image(500, 100, 'button1');
-
-    this.player = this.physics.add.image(32, 32, 'characters', 0);
-    this.player.setScale(2);
-
-    this.cursors = this.input.keyboard.createCursorKeys();
-}
-
-function update() {
-    this.player.setVelocity(0);
-
-    if (this.cursors.left.isDown) {
-        this.player.setVelocityX(-160);
-    } else if (this.cursors.right.isDown) {
-        this.player.setVelocityX(160);
-    }
-
-    if (this.cursors.up.isDown) {
-        this.player.setVelocityY(-160);
-    } else if (this.cursors.down.isDown) {
-        this.player.setVelocityY(160);
-    }
-}
+    
