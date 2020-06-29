9f8848f18416c193989c719a311704ff64fc964d
Pietrzi <pietrzi@op.pl>
2020-06-29 13:18:55 +0200

    client side done

--- phaserJS/2d-mmorpg/client/src/scenes/GameScene.js
+++ phaserJS/2d-mmorpg/client/src/scenes/GameScene.js
@@ -20,12 +20,153 @@ export default class GameScene extends Phaser.Scene {
     this.listenForSocketEvents();
   }
 
+  listenForSocketEvents() {
+    // spawn player game objects
+    this.socket.on('currentPlayers', (players) => {
+      Object.keys(players).forEach((id) => {
+        if (players[id].id === this.socket.id) {
+          this.createPlayer(players[id], true);
+          this.addCollisions();
+        } else {
+          this.createPlayer(players[id], false);
+        }
+      });
+    });
+
+    // spawn monster game objects
+    this.socket.on('currentMonsters', (monsters) => {
+      Object.keys(monsters).forEach((id) => {
+        this.spawnMonster(monsters[id]);
+      });
+    });
+
+    // spawn chest game objects
+    this.socket.on('currentChests', (chests) => {
+      Object.keys(chests).forEach((id) => {
+        this.spawnChest(chests[id]);
+      });
+    });
+
+    // spawn player game object
+    this.socket.on('spawnPlayer', (player) => {
+      this.createPlayer(player, false);
+    });
+
+    // a player has moved
+    this.socket.on('playerMoved', (player) => {
+      this.otherPlayers.getChildren().forEach((otherPlayer) => {
+        if (player.id === otherPlayer.id) {
+          otherPlayer.flipX = player.flipX;
+          otherPlayer.setPosition(player.x, player.y);
+          otherPlayer.updateHealthBar();
+          otherPlayer.updateFlipX();
+          otherPlayer.playerAttacking = player.playerAttacking;
+          otherPlayer.currentDirection = player.currentDirection;
+          if (player.playerAttacking) {
+            otherPlayer.attack();
+          }
+        }
+      });
+    });
+
+    this.socket.on('chestSpawned', (chest) => {
+      this.spawnChest(chest);
+    });
+
+    this.socket.on('monsterSpawned', (monster) => {
+      this.spawnMonster(monster);
+    });
+
+    this.socket.on('chestRemoved', (chestId) => {
+      this.chests.getChildren().forEach((chest) => {
+        if (chest.id === chestId) {
+          chest.makeInactive();
+        }
+      });
+    });
+
+    this.socket.on('monsterRemoved', (monsterId) => {
+      this.monsters.getChildren().forEach((monster) => {
+        if (monster.id === monsterId) {
+          monster.makeInactive();
+          this.monsterDeathAudio.play();
+        }
+      });
+    });
+
+    this.socket.on('monsterMovement', (monsters) => {
+      this.monsters.getChildren().forEach((monster) => {
+        Object.keys(monsters).forEach((monsterId) => {
+          if (monster.id === monsterId) {
+            this.physics.moveToObject(monster, monsters[monsterId], 40);
+          }
+        });
+      });
+    });
+
+    this.socket.on('updateScore', (goldAmount) => {
+      this.events.emit('updateScore', goldAmount);
+    });
+
+    this.socket.on('updateMonsterHealth', (monsterId, health) => {
+      this.monsters.getChildren().forEach((monster) => {
+        if (monster.id === monsterId) {
+          monster.updateHealth(health);
+        }
+      });
+    });
+
+    this.socket.on('updatePlayerHealth', (playerId, health) => {
+      if (this.player.id === playerId) {
+        if (health < this.player.health) {
+          this.playerDamageAudio.play();
+        }
+        this.player.updateHealth(health);
+      } else {
+        this.otherPlayers.getChildren().forEach((player) => {
+          if (player.id === playerId) {
+            player.updateHealth(health);
+          }
+        });
+      }
+    });
+
+    this.socket.on('respawnPlayer', (playerObject) => {
+      if (this.player.id === playerObject.id) {
+        this.playerDeathAudio.play();
+        this.player.respawn(playerObject);
+      } else {
+        this.otherPlayers.getChildren().forEach((player) => {
+          if (player.id === playerObject.id) {
+            player.respawn(playerObject);
+          }
+        });
+      }
+    });
+
+    this.socket.on('disconnect', (playerId) => {
+      this.otherPlayers.getChildren().forEach((player) => {
+        if (playerId === player.id) {
+          player.cleanUp();
+        }
+      });
+    });
+
+    this.socket.on('invalidToken', () => {
+      window.alert('Token is no longer valid. Please login again.');
+      window.location.reload();
+    });
+  }
+
   create() {
     this.createMap();
     this.createAudio();
     this.createGroups();
     this.createInput();
     this.createGameManager();
+
+    // emit event to server that a new player joined
+    this.socket.emit('newPlayer', getCookie('jwt'));
   }
 
   update() {
